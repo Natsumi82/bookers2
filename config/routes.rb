88fe34333cfc3a63e7056f0243f4b220a22a3ddc
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-03-10 09:35:59 +0000

    20230310-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  devise_for :users
  root to: "homes#top"
+ get "about" => "homes#about" , as: "about"
+
 
  resources :books, only: [:new, :show,:create, :edit,:index,  :update,:destroy]
  resources :users, only: [:show,:edit,:update,:index,:update,:destroy]
